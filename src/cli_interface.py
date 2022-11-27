3cdb0466ee5babbae886aecf039cb3ecf0e6e81d
Geovanna Menegasse <gege.menegasse@gmail.com>
2022-11-26 23:33:50 -0300

    dfghj

--- src/cli_interface.py
+++ src/cli_interface.py
@@ -1,63 +1,72 @@
-import sys
 import socket
+
 from time import sleep
 
-sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SEP = ':'
+
+COMMAND_C = 'C'
+COMMAND_D = 'D'
+COMMAND_I = 'I'
+COMMAND_F = 'F'
+COMMAND_T = 'T'
+COMMAND_E = 'E'
+COMMAND_S = 'S'
+
+sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if __name__ == '__main__':
 
     while True:
         entrada = input().split(' ')
 
-        if entrada[2] == 'C':
+        if entrada[2] == COMMAND_C:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
             end_IP_2 = entrada[3]
             porto_2  = entrada[4]
             nome     = entrada[5]
 
-            msg = entrada[2] + ':' + end_IP_2 + ':' + porto_2 + ':' + nome
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            msg = entrada[2] + SEP + end_IP_2 + SEP + porto_2 + SEP + nome
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
 
-        if entrada[2] == 'D':
+        if entrada[2] == COMMAND_D:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
             end_IP_2 = entrada[3]
             porto_2  = entrada[4]
 
-            msg = entrada[2] + ':' + end_IP_2 + ':' + porto_2
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            msg = entrada[2] + SEP + end_IP_2 + SEP + porto_2
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
 
-        if entrada[2] == 'I':
+        if entrada[2] == COMMAND_I:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
             
             msg = entrada[2]
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
 
-        if entrada[2] == 'F':
+        if entrada[2] == COMMAND_F:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
 
             msg = entrada[2]
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
             
-        if entrada[2] == 'T':
+        if entrada[2] == COMMAND_T:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
 
             msg = entrada[2]
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
             
-        if entrada[2] == 'E':
+        if entrada[2] == COMMAND_E:
             end_IP_1 = entrada[0]
             porto_1  = entrada[1]
             mensagem = entrada[3]
-            end_IP_2 = entrada[4]
-            porto_2  = entrada[5]
+            nome_destino = entrada[4]
 
-            msg = entrada[2] + ':' + mensagem + ':' + end_IP_2 + ':' + porto_2   
-            sck.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
+            msg = entrada[2] + SEP + mensagem + SEP + nome_destino   
+            sckt.sendto(msg.encode("latin-1"),(end_IP_1, int(porto_1)))
             
-        if entrada[2] == 'S':
+        if entrada[2] == COMMAND_S:
             sleep(float(entrada[3]))
--- src/roteador.py
+++ src/roteador.py
@@ -1,91 +1,135 @@
 import sys
 import socket
+
 from threading import Thread, Timer
 from time import sleep
 
-identificador = sys.argv[1]
-porto = sys.argv[2] 
-vizinhos = []
-tabela_roteamento = {identificador : (identificador, 0, identificador)}
+MY_IP = 'localhost'
+INFINITY = 16
+SEP = ':'
 
-print("Roteador " + identificador + " iniciado...")
+COMMAND_C = 'C'
+COMMAND_D = 'D'
+COMMAND_I = 'I'
+COMMAND_F = 'F'
+COMMAND_T = 'T'
+COMMAND_E = 'E'
 
-sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sck.bind(('localhost', int(porto)))
+IDENTIFICADOR = sys.argv[1]
+PORTO = sys.argv[2] 
+
+tabela_vizinhos = []
+tabela_roteamento = {IDENTIFICADOR : (IDENTIFICADOR, 0, IDENTIFICADOR)}
+
+# print("Roteador " + identificador + " iniciado...")
+
+sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sckt.bind((MY_IP, int(PORTO)))
 
 
 def adiciona_vizinho(msg):
     nome, ip, port = msg[3], msg[1], msg[2]
-    vizinhos.append((nome, ip, port))
+    tabela_vizinhos.append((nome, ip, port))
     tabela_roteamento[nome] = (nome, 1, nome)
 
 def remove_vizinho(msg):
-    global vizinhos
+    global tabela_vizinhos
+
     aux = []
     nome_vizinho = ''
 
-    for vizinho in vizinhos:
+    for vizinho in tabela_vizinhos:
         if vizinho[1] != msg[1] or vizinho[2] != msg[2]:
             aux.append(vizinho)
         else:
             nome_vizinho = vizinho[0]
-    vizinhos = aux            
+
+    tabela_vizinhos = aux            
 
     if nome_vizinho in tabela_roteamento.keys():
-        tabela_roteamento[nome_vizinho] = (tabela_roteamento[nome_vizinho][0], 16, tabela_roteamento[nome_vizinho][2])
-        new_msg = 'D:' + 'localhost' + ':' + str(porto)
-        sck.sendto(new_msg.encode("latin-1"),(msg[1], int(msg[2])))
+        tabela_roteamento[nome_vizinho] = (tabela_roteamento[nome_vizinho][0], INFINITY, tabela_roteamento[nome_vizinho][2])
+        new_msg = COMMAND_D + SEP + MY_IP + SEP + str(PORTO)
+        sckt.sendto(new_msg.encode("latin-1"),(msg[1], int(msg[2])))
     
     for rota in tabela_roteamento.values():
         if nome_vizinho == rota[2]:
-            tabela_roteamento[rota[0]] = (rota[0], 16, tabela_roteamento[rota[0]][2])
+            tabela_roteamento[rota[0]] = (rota[0], INFINITY, tabela_roteamento[rota[0]][2])
 
 def manda_anuncio():
     sleep(1)
-    anuncio = monta_anuncio(tabela_roteamento, identificador)
-    for vizinho in vizinhos:
-        if vizinho[0] != identificador:
-            sck.sendto(anuncio.encode("latin-1"),(vizinho[1], int(vizinho[2])))
+    anuncio = monta_anuncio(tabela_roteamento, IDENTIFICADOR)
+    for vizinho in tabela_vizinhos:
+        if vizinho[0] != IDENTIFICADOR:
+            sckt.sendto(anuncio.encode("latin-1"),(vizinho[1], int(vizinho[2])))
 
 def monta_anuncio(tb_roteamento, nome_roteador):
     tabela_anuncio = []
     for rota in tb_roteamento.values():
         tabela_anuncio.append((rota[0], rota[1]))
 
-    mensagem = '11111' + ':' + nome_roteador + ':' + str(len(tabela_anuncio))
+    mensagem = '11111' + SEP + nome_roteador + SEP + str(len(tabela_anuncio))
     for (dest, cust) in tabela_anuncio:
-        mensagem = mensagem + ':' + dest + ':' + str(cust)
+        mensagem = mensagem + SEP + dest + SEP + str(cust)
     
     return mensagem
 
 def encerra_execucao():
     exit(1)
 
-def imprime_tabela(identificador):
-    print(identificador)
+def imprime_tabela():
+    print(IDENTIFICADOR)
     for rota in tabela_roteamento.values():
         print(rota[0], end=" ")
         print(rota[1], end=" ")
         print(rota[2])
     print('\n')
 
-def envia_mensagem(msg):
-    # print(msg)
+def envia_mensagem(msg, nome_origem):
     mensagem = msg[1]
-    ip = msg[2]
-    port = msg[3]        
-    sck.sendto(mensagem.encode("latin-1"),(ip, int(port)))
+    nome_destino = msg[2]
+    
+    if len(msg) < 4:
+        nome_origem = IDENTIFICADOR
+    else:
+        nome_origem = msg[3]
 
-def recebe_anuncio(msg, recvd):
+    if nome_destino not in tabela_roteamento.keys():
+        print("X " +  mensagem + " de " + nome_origem + " para " + nome_destino)
+        return
+
+    nome_next = tabela_roteamento[nome_destino][2] 
+
+    if nome_destino == IDENTIFICADOR:
+        print("R " +  mensagem + " de " + nome_origem + " para " + nome_destino)
+        return
+    else:
+        print("E " +  mensagem + " de " + nome_origem + " para " + nome_destino + " através de " + nome_next)
+        
+    ip_next = ''
+    port_next = None
+
+    for vizinho in tabela_vizinhos:
+        if vizinho[0] == nome_next:
+            ip_next = vizinho[1]
+            port_next = vizinho[2]
+            break
+
+    if nome_origem != '':
+        nome_origem = SEP + nome_origem
+
+    new_msg = COMMAND_E + SEP + mensagem + SEP + nome_destino + nome_origem
+    sckt.sendto(new_msg.encode("latin-1"),(ip_next, int(port_next)))
+
+def recebe_anuncio(msg, emissor):
     achei = False
-    for vizinho in vizinhos:
+    for vizinho in tabela_vizinhos:
         if vizinho[0] == msg[1]:
             achei = True
-    if achei == False and msg[1] != identificador:
-        if recvd[1][0] == '127.0.0.1':
-            vizinhos.append((msg[1],'localhost',str(recvd[1][1])))
+    if achei == False and msg[1] != IDENTIFICADOR:
+        if emissor[1][0] == '127.0.0.1':
+            tabela_vizinhos.append((msg[1],'localhost',str(emissor[1][1])))
         else:
-            vizinhos.append((msg[1],str(recvd[1][0]),str(recvd[1][1])))
+            tabela_vizinhos.append((msg[1],str(emissor[1][0]),str(emissor[1][1])))
 
     tabela_anuncio = []
     for i in range(3, len(msg), 2):
@@ -111,28 +155,28 @@ while True:
     t = Timer(1.0, manda_anuncio)
     t.start()
 
-    recvd = sck.recvfrom(50)
-    msg = (recvd[0].decode("utf-8")).split(':')
+    emissor = sckt.recvfrom(50)
+    msg = (emissor[0].decode("utf-8")).split(SEP)
 
-    if msg[0] == 'C': 
+    if msg[0] == COMMAND_C: 
         adiciona_vizinho(msg)
 
-    elif msg[0] == 'D':
+    elif msg[0] == COMMAND_D:
         remove_vizinho(msg)
 
-    elif msg[0] == 'I':
+    elif msg[0] == COMMAND_I:
         manda_anuncio()
 
-    elif msg[0] == 'F':
+    elif msg[0] == COMMAND_F:
         encerra_execucao()
 
-    elif msg[0] == 'T':
-        imprime_tabela(identificador)
+    elif msg[0] == COMMAND_T:
+        imprime_tabela()
 
-    elif msg[0] == 'E':
-        envia_mensagem(msg)
+    elif msg[0] == COMMAND_E:
+        envia_mensagem(msg, '')
 
     else:
-        recebe_anuncio(msg, recvd)
+        recebe_anuncio(msg, emissor)
     
     t.cancel()
